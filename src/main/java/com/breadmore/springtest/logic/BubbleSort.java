2dc7ed5c9c07565023a25031d8e844c5e60aa7b4
BM <lepo3@naver.com>
2022-06-22 21:09:15 +0900

    Apply #1 - spring dependencies to gradle
    스프링을 사용할 준비

--- src/main/java/com/breadmore/springtest/logic/BubbleSort.java
+++ src/main/java/com/breadmore/springtest/logic/BubbleSort.java
@@ -1,8 +1,11 @@
 package com.breadmore.springtest.logic;
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class BubbleSort<T extends Comparable<T>> implements Sort<T>{
 
     @Override
--- src/main/java/com/breadmore/springtest/service/SortService.java
+++ src/main/java/com/breadmore/springtest/service/SortService.java
@@ -1,14 +1,17 @@
 package com.breadmore.springtest.service;
 
 import com.breadmore.springtest.logic.Sort;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class SortService {
 
     private final Sort<String> sort;
 
-    public SortService(Sort<String> sort) {
+    public SortService(@Qualifier("bubbleSort") Sort<String> sort) {
         this.sort = sort;
         System.out.println("name : " + sort.getClass().getName());
     }
