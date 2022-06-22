a2545ef4ce0f9740b1bed78d4d24bedff29fb942
BM <lepo3@naver.com>
2022-06-22 21:36:20 +0900

    Config #1 - webmvc
    webmvc 도입

--- src/main/java/com/breadmore/springtest/config/Config.java
+++ src/main/java/com/breadmore/springtest/config/Config.java
@@ -2,7 +2,9 @@ package com.breadmore.springtest.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.breadmore.springtest")
 @Configuration
 public class Config {
