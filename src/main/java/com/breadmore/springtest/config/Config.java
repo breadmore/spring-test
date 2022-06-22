ba945ae691cb4d2d5bc6d8a5b6ca010dc3fa21e2
BM <lepo3@naver.com>
2022-06-22 21:09:49 +0900

    Config #1 - spring  configuration
    기초 설정
    루트 패키지 이하에 빈이 있는지 스캔

---
+++ src/main/java/com/breadmore/springtest/config/Config.java
@@ -0,0 +1,10 @@
+package com.breadmore.springtest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.breadmore.springtest")
+@Configuration
+public class Config {
+
+}
